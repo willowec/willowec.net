b85e6a6a32436fecbe6568c908f55479c3ac6d65
William Edward Cunningham <william.e.cunningham@maine.edu>
2023-09-01 22:48:54 -0400

    Added games page with responsive grid

---
+++ js/games.js
@@ -0,0 +1,60 @@
+$(window).on('load', function() {
+    // Add custom elements to the DOM
+    customElements.define("itch-game", ItchGame)
+
+    // Load up all the games
+    loadGames();
+});
+
+class ItchGame extends HTMLElement {
+    /*
+    Required attributes:
+        user: the itch developer's username
+        game: the game to display
+    */
+    constructor() {
+        super();
+
+        // get attributes and initialize relevant ids
+        this.user = this.getAttribute("user");
+        this.game = this.getAttribute("game");
+        this.id = "game-" + this.user + "-" + this.game;
+        this.content_id = this.id + '-content';
+
+        // request game information from itch
+        let thisctx = this;
+        Itch.getGameData({
+            user: this.user,
+            game: this.game,
+            onComplete: function(data) {
+                thisctx.displayGameInformation(data);
+            }
+        });
+    }
+
+    // Displays information in the 'data' object
+    displayGameInformation(data) {
+        this.innerHTML = 
+        `
+        <div class="itch-game">
+            <a href="https://${this.user}.itch.io/${this.game}">
+                <div ${this.content_id}>
+                    <h2>${data.title}</h2>
+                    <img src="${data.cover_image}">
+                </div>
+                <div class="itch-game-popup">
+                    <h1>Play on Itch.io</h1>
+                </div>
+            </a>
+        </div>
+        `;
+    }
+}
+
+function loadGames() {
+    $.getJSON("/content/games.json", function(games) {
+        $.each(games, function(i, game) {
+            $("#games-container").append(`<div class=card><itch-game user="willc-squared" game="${game}"></itch-game></div>`);
+        })
+    })
+}
